5b7300a66e212a0cb59076dbe1e868606f093bf7
Arthur Alves <arthuralves.p@gmail.com>
2015-07-21 20:02:59 +0300

    Allow basic Loading View customization. Change loading label text and color, change progress bar color.

--- PagingListViewProject/PagingListView/src/main/java/com/paging/listview/LoadingView.java
+++ PagingListViewProject/PagingListView/src/main/java/com/paging/listview/LoadingView.java
@@ -2,10 +2,16 @@ package com.paging.listview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class LoadingView extends LinearLayout {
+
+    public ProgressBar  progressBar;
+    public TextView     loadingLabel;
 
     public LoadingView(Context context) {
         super(context);
@@ -18,7 +24,10 @@ public class LoadingView extends LinearLayout {
     }
 
     private void init() {
-        inflate(getContext(), R.layout.loading_view, this);
+        View view = inflate(getContext(), R.layout.loading_view, this);
+
+        progressBar = (ProgressBar) view.findViewById(R.id.video_item_image);
+        loadingLabel = (TextView) view.findViewById(R.id.video_item_label);
     }
 
 
